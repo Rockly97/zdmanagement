f717f2d457041261e2a777830e2047e449087011
wwclearlove <1280488753@qq.com>
2019-11-12 17:58:12 +0800

    wyc

---
+++ src/main/java/com/zdxt/model/ContactWe.java
@@ -0,0 +1,70 @@
+package com.zdxt.model;
+
+import java.io.Serializable;
+
+public class ContactWe implements Serializable {
+    private String id;
+
+    public ContactWe() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getComent() {
+        return coment;
+    }
+
+    public void setComent(String coment) {
+        this.coment = coment;
+    }
+
+    private String name;
+
+
+    private String email;
+
+
+    private String phone;
+    private String coment;
+
+    @Override
+    public String toString() {
+        return "ContactWe{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                ", coment='" + coment + '\'' +
+                '}';
+    }
+}
